66ae8fbc57fac6029d46829ce6bde941ddd31970
victor <victorsalazarsalinas@gmail.com>
2026-03-05 07:18:29 -0600

    Práctica 01 correcion:mayuscula

--- src/unidad1/drone/Flota_Drone.java
+++ src/unidad1/drone/Flota_Drone.java
@@ -1,13 +1,13 @@
 package unidad1.drone;
 
-public class flota_drone {
+class Flota_Drone {
     public static void main(String[] args) {
         // Crear 3 drones
         Drone d1 = new Drone(1, 100);
         Drone d2 = new Drone(2, 80);
         Drone d3 = new Drone(3, 60);
 
-        // Mestado inicial
+        // estado inicial
         d1.mostrarEstado();
         d2.mostrarEstado();
         d3.mostrarEstado();
