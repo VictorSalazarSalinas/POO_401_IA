7ebd6bfc89eebfd53b65878f73b5e0f4abd20e5f
victor <victorsalazarsalinas@gmail.com>
2026-03-25 07:41:32 -0600

    Version 2 manejo de instancias

--- src/unidad2/SensorIA/Main.java
+++ src/unidad2/SensorIA/Main.java
@@ -7,9 +7,24 @@ public class Main {
     public static void main(String[] args) {
 
         List<SensorIA> percepcionHub = new ArrayList<>();
-        percepcionHub.add(new SensorLidar());
-        percepcionHub.add(new SensorVision());
-        percepcionHub.add(new SensorUltrasonido());
+
+        SensorLidar obje1 = new SensorLidar();
+        SensorLidar obje2 = new SensorLidar();
+
+        SensorVision obje3 = new SensorVision();
+        SensorVision obje4 = new SensorVision();
+
+        SensorUltrasonido obje5 = new SensorUltrasonido();
+        SensorUltrasonido obje6 = new SensorUltrasonido();
+
+
+        percepcionHub.add(obje1);
+        percepcionHub.add(obje2);
+        percepcionHub.add(obje3);
+        percepcionHub.add(obje4);
+        percepcionHub.add(obje5);
+        percepcionHub.add(obje6);
+
 
         System.out.println("Iniciando Sistema version 1");
 
