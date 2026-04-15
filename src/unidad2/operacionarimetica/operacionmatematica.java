3ad08150ba2bcace6315fd8f10652ed34d729bd7
victorsalazarsalinas <victorsalazarsalinas@gmail.com>
2026-04-15 15:12:42 -0600

    Ejercicio_Sentencias y Expresiones

---
+++ src/unidad2/operacionarimetica/operacionmatematica.java
@@ -0,0 +1,17 @@
+package unidad2.operacionarimetica;
+
+public class operacionmatematica {
+    public static void main(String[] args) {
+        // sentencia de declaracion con expresion incluida
+        int producto = 8 * 12;
+
+        // sentencia de declaracion
+        int cociente;
+
+        // sentencia de asignacion con expresion
+        cociente = 100 / 5;
+
+        System.out.println("el producto es: " + producto);
+        System.out.println("el cociente es: " + cociente);
+    }
+}
