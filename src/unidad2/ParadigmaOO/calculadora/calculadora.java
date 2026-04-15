4244b626eec18182f91445a37e7fb5612d66ee23
victorsalazarsalinas <victorsalazarsalinas@gmail.com>
2026-04-15 13:02:13 -0600

    Paradigma OO :Práctica 6

---
+++ src/unidad2/ParadigmaOO/calculadora/calculadora.java
@@ -0,0 +1,10 @@
+package unidad2.ParadigmaOO.calculadora;
+
+class calculadora {
+    int sumar(int a, int b) {
+        return a + b; }
+    int sumar(int a, int b, int c) {
+        return a + b + c; }
+    double sumar(double a, double b) {
+        return a + b; }
+}
