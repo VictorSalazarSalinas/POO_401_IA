225aee9a7ff7882fa2e81494adbf82e9ba36f5eb
victorsalazarsalinas <victorsalazarsalinas@gmail.com>
2026-04-15 14:20:45 -0600

    Ejercicio_Ciclo_flujo

---
+++ src/unidad2/recetacocina/recetadecocina.java
@@ -0,0 +1,14 @@
+package unidad2.recetacocina;
+
+public class recetadecocina {
+    public static void main(String[] args) {
+        int ingredientespreparados = 5;
+        int tiempodecocina = 30;
+
+        System.out.println("paso 1: prepara los ingredientes.");
+        System.out.println("cantidad de ingredientes: " + ingredientespreparados);
+        System.out.println("paso 2: cocina el platillo.");
+        System.out.println("tiempo en minutos: " + tiempodecocina);
+        System.out.println("el platillo esta listo.");
+    }
+}
